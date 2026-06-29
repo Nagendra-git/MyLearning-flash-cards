b602febcd2b017524ed3ffeab35f81bfccef0661
Nagendra <nagendra.burusu@thoughtclan.com>
2026-06-29 13:47:17 +0530

    added java misc, java string and java object

---
+++ src/data/javaObjectCards.js
@@ -0,0 +1,16 @@
+import javaObjectClass from "./cards/javaObject/ObjectClass.html?raw";
+import stringtoStringHtml from "./cards/javaObject/toString.html?raw";
+
+const javaObjectCards = [
+    {
+        question : "Java Object Class",
+        answerHtml : javaObjectClass
+    },
+    {
+        question : "toString()",
+        answerHtml : stringtoStringHtml
+    }
+
+]
+
+export default javaObjectCards;
