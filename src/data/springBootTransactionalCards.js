03a31e9c55b7c4194ff600646b55286572dee40c
Nagendra <nagendra.burusu@thoughtclan.com>
2026-06-30 12:39:10 +0530

    Transaction propagations

--- src/data/springBootTransactionalCards.js
+++ src/data/springBootTransactionalCards.js
@@ -2,6 +2,11 @@ import springBootTransactionHTML from "./cards/springBootTransaction/springBootT
 import transactionPropagationHTML from "./cards/springBootTransaction/transactionPropagations.html?raw";
 import requiredPropagationHTML from "./cards/springBootTransaction/requiredPropargation.html?raw";
 import requiredNewPropagationHTML from "./cards/springBootTransaction/requiredNewPropagation.html?raw";
+import supportPropagationHTML from "./cards/springBootTransaction/supportsPropagation.html?raw";
+import mandatoryPropagationHTML from "./cards/springBootTransaction/mandatoryPropagation.html?raw";
+import notSupportPropagationHTML from "./cards/springBootTransaction/notSupportPropagation.html?raw";
+import neverPropagationHTML from "./cards/springBootTransaction/neverPropagation.html?raw";
+import nestedPropagationHTML from "./cards/springBootTransaction/nestedPropagation.html?raw";
 
 const springBootTransactionalCards = [
     {
@@ -20,7 +25,26 @@ const springBootTransactionalCards = [
         question : "REQUIRED_NEW Propagation",
         answerHtml : requiredNewPropagationHTML
     },
-
+    {
+        question : "SUPPORTS Propagation",
+        answerHtml : supportPropagationHTML
+    },
+    {
+        question : "MANDATORY Propagation",
+        answerHtml : mandatoryPropagationHTML
+    },
+    {
+        question : "NOT_SUPPORTS Propagation",
+        answerHtml : notSupportPropagationHTML
+    },
+    {
+        question : "NEVER Propagation",
+        answerHtml : neverPropagationHTML
+    },
+    {
+        question : "NESTED Propagation",
+        answerHtml : nestedPropagationHTML
+    },
 ];
 
 export default springBootTransactionalCards;
