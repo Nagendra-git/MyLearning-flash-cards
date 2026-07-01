232715d185d95acdcc81731519626137e1517e75
Nagendra <nagendra.burusu@thoughtclan.com>
2026-07-01 17:10:56 +0530

    added exception learning cards

---
+++ src/data/exceptionMiscCards.js
@@ -0,0 +1,35 @@
+import exceptionFundamentalsHtml from "./cards/exception/exceptionFundamentals.html?raw";
+import checkedVsUnchecedExceptionHtml from "./cards/exception/checkedVsUnchecedException.html?raw";
+import exceptionKeywordsHtml from "./cards/exception/exceptionKeywords.html?raw";
+import exceptionFlowHtml from "./cards/exception/exceptionFlow.html?raw";
+import springBootExceptionFlowHtml from "./cards/exception/springBootExceptionFlow.html?raw"
+import handlingExceptionHtml from "./cards/exception/handlingException.html?raw";
+
+const exceptionMiscCards = [
+    {
+        question: "Exception Fundamentals",
+        answerHtml: exceptionFundamentalsHtml
+    },
+    {
+        question : "Checked vs Unchecked Exceptions",
+        answerHtml : checkedVsUnchecedExceptionHtml
+    },
+    {
+        question : "Exception keywords",
+        answerHtml : exceptionKeywordsHtml
+    },
+    {
+        question : "Exception Flow",
+        answerHtml : exceptionFlowHtml
+    },
+    {
+        question :"Spring Boot Exception Flow",
+        answerHtml : springBootExceptionFlowHtml
+    },
+    {
+        question : "Handling Exceptions",
+        answerHtml : handlingExceptionHtml
+    }
+];
+
+export default exceptionMiscCards;
