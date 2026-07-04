d5d42a6c4fcfffe97174a0f797fd382b7d8cd718
Nagendra <nagendra.burusu@thoughtclan.com>
2026-07-04 19:02:19 +0530

    added cards for java stream and collectrs class

--- src/data/javaMiscCards.js
+++ src/data/javaMiscCards.js
@@ -1,10 +1,33 @@
 import callbyValueVsCallbyreference from "./cards/javaMisc/CallbyValueVsCallbyreference.html?raw";
-
+import comaprableVsComparatorHtml from "./cards/javaMisc/comaprableVsComparator.html?raw";
+import javaStreamsHtml from "./cards/javaMisc/javaStreams.html?raw";
+import javaCollectorsHtml from "./cards/javaMisc/javaCollectors.html?raw";
+import javaTerminalOperationsHtml from "./cards/javaMisc/javaTerminalOperations.html?raw";
+import javaIntermediateOperationsHtml from "./cards/javaMisc/javaIntermediateOperations.html?raw";
 const javaMiscCards = [
     {
         question : "call by Value Vs Call by reference",
         answerHtml : callbyValueVsCallbyreference
-
+    },
+    {
+        question : "Comparable Vs Comparator",
+        answerHtml : comaprableVsComparatorHtml
+    },
+    {
+        question : "Java Streams",
+        answerHtml : javaStreamsHtml
+    },
+    {
+        question : "Java Collectors",
+        answerHtml : javaCollectorsHtml
+    },
+    {
+        question : "Java Terminal Opertaions",
+        answerHtml : javaTerminalOperationsHtml
+    },
+    {
+        question : "Java Intermediate Operations",
+        answerHtml : javaIntermediateOperationsHtml
     }
 ]
 
