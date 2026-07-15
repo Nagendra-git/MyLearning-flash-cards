53e2c9c71b11810bbc2980f98d34d1dac61d48a1
Nagendra <nagendra.burusu@thoughtclan.com>
2026-07-15 09:01:59 +0530

    Added linear and recursive binary serch algo

--- src/data/algorithmsCards.js
+++ src/data/algorithmsCards.js
@@ -1,10 +1,15 @@
 import binarySearchHtml from "./cards/algorithm/BinarySearch.html?raw";
-
+import linearSearchhtml from "./cards/algorithm/linearSearch.html?raw";
 const algorithmsCards = [
+  {
+    question : "Linear Search O(n)",
+    answerHtml: linearSearchhtml
+  },
   {
     question: "Binary Search O(log n)",
     answerHtml: binarySearchHtml
-  }
+  },
+  
 ];
 
 export default algorithmsCards;
