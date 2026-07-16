e4f396042c8d8a8fa16e888fe749de21e56b1a1a
Nagendra <nagendra.burusu@thoughtclan.com>
2026-07-16 09:10:18 +0530

    Sql aggregation and filter query cards

--- src/data/sqlCards.js
+++ src/data/sqlCards.js
@@ -1,10 +1,20 @@
 import sqlFundamentalsHtml from "./cards/sql/sqlFundamentals.html?raw";
+import sqlAggregateFunctionHtml from "./cards/sql/sqlAggregateFunction.html?raw";
+import filteringAndSortingHtml from "./cards/sql/filteringAndSorting.html?raw";
 
 const sqlCards = [
      {
         question : "SQL Fundamentals",
         answerHtml : sqlFundamentalsHtml
     },
+    {
+        question : "SQL aggregate function",
+        answerHtml: sqlAggregateFunctionHtml
+    },
+    {
+        question : "Filtering and sorting",
+        answerHtml : filteringAndSortingHtml
+    }
 ];
 
 export default sqlCards;
